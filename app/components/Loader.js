748f02f30229d4ea8328a559b8a84d0b1343a788
Pavel <pavepy@yandex.ru>
2018-03-19 00:35:14 +0300

    Loader fixes

--- app/components/Loader.js
+++ app/components/Loader.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Loader extends Component {
 	render() {
+		const { fixed } = this.props;
+		const className = fixed ? 'preloader preloader--fixed' : 'preloader';
 		return (
-			<div className="preloader">
+			<div className={className}>
 				<div className="preloader-spin" />
 			</div>
 		);
 	}
 }
+
+Loader.propTypes = {
+	fixed: PropTypes.bool
+};
 
 export default Loader;
