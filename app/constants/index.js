3385de51600c341b7a116460ff060ca1dd8afbae
Carduelis <pavepy@gmail.com>
2018-03-20 16:31:09 +0300

    new constants

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,7 +1,16 @@
-// export const GOOGLE_API_KEY = 'AIzaSyB2tvNcl5XrkduBGptWyaU9QJHsSMNRXG4';
-
-export const GOOGLE_API_KEY = 'AIzaSyB1ZiBxTH7tTqqIMqYXI6A0gdHKkG9BcA0'; // Misha's
-export const POKEMON_GITHUB_IMAGE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+export const POKEMON_GITHUB_IMAGE_URL =
+	'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 export const API_URL = 'https://pokeapi.co/api/v1/';
-
 export const BACKEND_ROOT = 'http://127.0.0.1/junction/public/';
+export const HARD_CACHE_IS_ON = `
+	======================
+	|  HARD CACHE IS ON  |
+	======================
+	`;
+
+export const NO_FILTERING_ERROR = `
+	=======================================================
+	|  NO LOADING, CAUSE API DOES NOT PROVIDE FILTERING.  |
+	|  PRELOAD AND CACHE ALL POKEMONS VIA CHUNK-LOADING.  |
+	=======================================================
+`;
